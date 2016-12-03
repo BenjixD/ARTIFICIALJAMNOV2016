e4431996fa83e4ce8d64bd942c641c558486f86a
Benjamin Zhao <yl4zhao@uwaterloo.ca>
2016-12-03 17:17:08 -0500

    Pushing dragon and human interaction

---
+++ Assets/playerStats.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerStats : MonoBehaviour {
+
+    public int playerLife = 3;
+    public float playerShield = 100f;
+    public bool shieldActive = false;
+
+
+}
