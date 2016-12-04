e22c29a658843927e73db48699a5c1dacc50c6c8
RhainyLeo <liaohenry2310@gmail.com>
2016-12-03 20:24:29 -0500

    nothing01

--- Assets/playerStats.cs
+++ Assets/playerStats.cs
@@ -4,8 +4,33 @@ using System.Collections;
 public class playerStats : MonoBehaviour {
 
     public int playerLife = 3;
-    public float playerShield = 100f;
+    public float playerShield;
+    public float maxShield = 300f;
     public bool shieldActive = false;
+    public bool canRegen = false;
 
+    void Start()
+    {
+        playerShield = maxShield;
+    }
 
+    void Update()
+    {
+        if (shieldActive)
+        {
+            playerShield -= 1;
+        }
+        else
+        {
+            if(playerShield < maxShield && canRegen)
+            {
+                playerShield += 1;
+            }
+        }
+    }
+
+    void setRegenTrue()
+    {
+        canRegen = true;
+    }
 }
