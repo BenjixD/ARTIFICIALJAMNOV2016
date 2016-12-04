9d4c7c1900d86cd4537f01266aaad926589552a1
RhainyLeo <liaohenry2310@gmail.com>
2016-12-04 18:35:41 -0500

    meteors now damage player, added music

---
+++ Assets/PlayerMeteor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMeteor : MonoBehaviour {
+
+    playerStats playerStatScript;
+
+    void Start()
+    {
+        playerStatScript = this.gameObject.GetComponent<playerStats>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (LayerMask.LayerToName(other.gameObject.layer) == "Meteor")
+        {
+            playerStatScript.playerTakeDamage();
+        }
+    }
+}
